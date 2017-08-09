07e35f3b557042c4dea954dd04a66290b958fe6a
Yi Mu <1215216426@qq.com>
2017-08-09 13:28:16 +0800

    add some tips

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
-integer=int(input('Please enter the integer: '))
-if integer >= 0:
-	print(integer)
-else: 
-	print(-integer)
+#-*- coding: utf-8 -*-
+name=input('Before check your account, please enter your name first: ')
+month=int(input("The month you'd like to check: "));
+s='亲爱的%s，你好！ 你%d月的话费是%.2f，余额是%.2f。'%(name,month,58.8,3.2)
+print(s)
+
